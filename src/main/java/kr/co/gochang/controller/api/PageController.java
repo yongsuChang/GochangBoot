7422e01e5026aa28244bf452ce43ad9f0159d1ca
yongsuChang <77391211+yongsuChang@users.noreply.github.com>
2021-07-26 15:20:02 +0900

    [feat] Added main page
    - Added main page's html and js
    
    - 메인 페이지 html, js 추가

---
+++ src/main/java/kr/co/gochang/controller/api/PageController.java
@@ -0,0 +1,14 @@
+package kr.co.gochang.controller.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+    @RequestMapping(value = {"", "/main"})
+    public ModelAndView main(){
+        return new ModelAndView("/pages/main");
+    }
+}
